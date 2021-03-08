955613f736031275bce69915d0972a05b7e90919
Brigi <brigitta.rucz@yahoo.com>
2021-03-08 19:45:09 +0100

    edit customer

--- employee_app/views.py
+++ employee_app/views.py
@@ -21,15 +21,58 @@ def overview_customers(request):
 
 def edit_customer(request, customer_id):
     customer_id = int(customer_id)
+    customer = Profile.objects.get(id=customer_id)
 
+    # GET method for populating form
     if request.method == 'GET':
         try:
-            customer = Profile.objects.get(id=customer_id)
+            context = {
+                'customer': customer
+            }
         except Profile.DoesNotExist:
             # Todo: Add context with errors
             return render(request, 'employee_app/overview_customers.html')
 
-    return render(request, 'employee_app/edit_customer.html')
+    # POST method for editing user
+    if request.method == 'POST':
+
+        # Todo: Check if passwords match
+        customer.user.email = request.POST['email']
+
+        customer.customer_rank = request.POST['customer_rank']
+        customer.customer_phone_number = request.POST['customer_phone_number']
+        customer.customer_can_loan = True if request.POST['customer_can_loan'] == 'on' else False
+        
+        try:
+            with transaction.atomic():
+                customer.save()
+                # # create_user() hashes the password
+                # user = User.objects.create_user(email = post_email, 
+                #                                 username = post_username, 
+                #                                 password = post_password, 
+                #                                 first_name = post_fname, 
+                #                                 last_name = post_lname)
+                # # create() does not hash 
+                # profile = Profile.objects.create(user = user, 
+                #                                 customer_rank = post_rank,
+                #                                 customer_phone_number = post_phone,
+                #                                 customer_token = '123token', 
+                #                                 customer_mfe = False, 
+                #                                 customer_can_loan = post_loan)
+            
+            context = {
+                'customers': Profile.objects.all(),
+                'accounts': Account.objects.all(),
+                'edited_user': customer
+            }
+
+            return render(request, 'employee_app/overview_customers.html', context)
+        except DatabaseError:
+            # Todo: print error message
+            return render(request, 'employee_app/create_customer.html')       
+            
+
+    return render(request, 'employee_app/edit_customer.html', context)
 
 def create_customer(request):
 
@@ -44,6 +87,7 @@ def create_customer(request):
         post_password = request.POST['password1']
         post_rank = request.POST['customer_rank']
         post_phone = request.POST['customer_phone_number']
+        post_loan = True if request.POST['customer_can_loan'] == 'on' else False
 
         try:
             with transaction.atomic():
@@ -59,7 +103,7 @@ def create_customer(request):
                                                 customer_phone_number = post_phone,
                                                 customer_token = '123token', 
                                                 customer_mfe = False, 
-                                                customer_can_loan = False)
+                                                customer_can_loan = post_loan)
             # Test exception:
             # if exception:
             #     raise exception
